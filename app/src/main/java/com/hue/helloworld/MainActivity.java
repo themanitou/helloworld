48b431ba57c6eac24c2d8e28b495a4706a6f5de2
Quan Nguyen <quan.nguyen@dektech.com.au>
2016-11-10 20:15:50 +1100

    Add Place Autocomplete feature

--- app/src/main/java/com/hue/helloworld/MainActivity.java
+++ app/src/main/java/com/hue/helloworld/MainActivity.java
@@ -3,17 +3,52 @@ package com.hue.helloworld;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements PlaceSelectionListener {
     public final static String EXTRA_MESSAGE = "com.hue.helloworld.MESSAGE";
+    private static final String LOG_TAG = "PlaceSelectionListener";
+    private EditText edtText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtText = (EditText) findViewById(R.id.edtText);
+
+        // Method #1
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        autocompleteFragment.setOnPlaceSelectedListener(this);
+        autocompleteFragment.setHint("Search a Location");
     }
 
+    @Override
+    public void onPlaceSelected(Place place) {
+        Log.i(LOG_TAG, "Place Selected: " + place.getName());
+        edtText.setText(place.getAddress().toString());
+    }
+
+    @Override
+    public void onError(Status status) {
+        Log.e(LOG_TAG, "onError: Status = " + status.toString());
+        Toast.makeText(this, "Place selection failed: " + status.getStatusMessage(),
+                Toast.LENGTH_SHORT).show();
+    }
     /* called when the button 'I want something' is clicked */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
